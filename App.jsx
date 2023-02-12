f267e7ed2738d6788201d3cb5ad1785f6c13e4e1
kareemzahoor <kareemzahoor79@gmail.com>
2023-02-12 13:37:59 +0500

    initial setup

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppNavigation from './navigation/navigation.js'
+
+const App = () => {
+  return (
+    <AppNavigation/>
+  )
+}
+
+export default App
---
+++ navigation/navigation.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import GetStarted from '../src/screens/GetStarted';
+import SignUp from '../src/screens/SignUp';
+import SignIn from '../src/screens/SignIn';
+import AdminHome from '../src/screens/AdminHome';
+import UserHome from '../src/screens/UserHome';
+
+
+const Stack = createNativeStackNavigator()
+
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName='GetStarted' screenOptions={{headerShown:false}}>
+      <Stack.Screen name="GetStarted" component={GetStarted} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="AdminHome" component={AdminHome} />
+      <Stack.Screen name="UserHome" component={UserHome} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
+
+export default AppNavigation
